0f308ae32dab67840c9ab784fb355e813226c7db
Nachiketha <98515406+Nachiketha237@users.noreply.github.com>
2023-12-18 14:47:11 +0530

    Update RFID.ino

--- 5th_sem/IOT_Lab/Lab4/RFID.ino
+++ 5th_sem/IOT_Lab/Lab4/RFID.ino
@@ -1,20 +1,20 @@
 #include<SoftwareSerial.h> 
 
-SoftwareSerialmySerial(9,10); 
+SoftwareSerial mySerial(9,10); 
 int count=0;                     //count=0 
-charinput[12];                    //characterarrayofsize12 
-booleanflag=0;                   //flag=0 
+char input[12];                    //characterarrayofsize12 
+boolean flag=0;                   //flag=0 
 void setup() {   
   Serial.begin(9600);                 //beginserialportwithbaudrate9600bps   
   mySerial.begin(9600); 
 } 
 
-voidloop() 
+void loop() 
 {   
   if(mySerial.available())
   {      
     count=0;      
-    while(mySerial.available()&&count<12)     //Read12charactersandstorethemin inputarray      
+    while(mySerial.available() && count<12)     //Read12charactersandstorethemin inputarray      
     {         
       input[count]=mySerial.read();         
       count++;         
