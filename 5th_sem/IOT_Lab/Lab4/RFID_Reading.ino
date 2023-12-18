ca05debc083248d02408ca71e293a92871df933b
Nachiketha <98515406+Nachiketha237@users.noreply.github.com>
2023-12-18 14:49:31 +0530

    Update RFID_Reading.ino

--- 5th_sem/IOT_Lab/Lab4/RFID_Reading.ino
+++ 5th_sem/IOT_Lab/Lab4/RFID_Reading.ino
@@ -1,10 +1,12 @@
-#include &lt;SoftwareSerial.h&gt; //Reading sinngle tag
+#include <SoftwareSerial.h> //Reading sinngle tag
 SoftwareSerial mySerial(9, 10);
 void setup()
 {
   mySerial.begin(9600); // Setting the baud rate of Software Serial Library  
   Serial.begin(9600);  //Setting the baud rate of Serial Monitor
- }void loop()
+ }
+
+void loop()
 {
     
  if(mySerial.available()&gt;0)
